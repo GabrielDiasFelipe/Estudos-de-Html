8109eea1c5a27bc6aaa8447e308e27a2e7f3f4c3
Kácio <63382277+Oicak@users.noreply.github.com>
2023-05-16 09:55:14 -0300

    scrol

--- Java Script/aula14/ex01/script.js
+++ Java Script/aula14/ex01/script.js
@@ -13,15 +13,22 @@ function cont(){
 
         document.getElementById('sec').style.display='none'
         document.getElementById('sec1').style.display='block'
+        document.getElementsByTagName('section')[0].style.overflowY='scroll'
+        if(passo <= 0 ){
+            alert('Passo Invalido! considerando PASSO como 1')
+            passo++
+        }
         if(ini < fim){
             for(var cont = ini; cont<=fim; cont += passo){
-            msg.innerHTML += `&#x1F449 ${cont}`
-        }
+                msg.innerHTML += `&#x1F449 ${cont}`
+            }
         }else{
             for(let cont = ini; cont >= fim ; cont-= passo ){
                 msg.innerHTML +=`${cont}&#x1F448 `
             }
-        }
+    }
+        
+        
         
         
     }
